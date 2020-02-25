cb87fe2c4f3ad044242b7fd293d72ae4441d7bb4
Alex-Umbelino <luiz.alex.umbelino@gmail.com>
2020-02-25 09:11:20 -0300

    parte 1

---
+++ Calculadora.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+void main()
+{
+    int n1=0,n2=0,s=0,sub=0,l;
+    float N1=0, N2=0,S=0;
+
+
+
+    printf("\nDigite um numero inteiro:");
+    scanf("%d",&n1);
+    printf("\nDigite o segundo numero inteiro:");
+    scanf("%d",&n2);
+
+    s=n1+n2;
+    printf("\n%d+%d=%d", n1, n2, s);
+
+
+
+
+}
