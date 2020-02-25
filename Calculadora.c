0d47677c96cdbf8bd2386bc1dfed5b5295b87903
Alex-Umbelino <luiz.alex.umbelino@gmail.com>
2020-02-25 09:14:24 -0300

    Concluido

--- Calculadora.c
+++ Calculadora.c
@@ -23,7 +23,41 @@ void main()
     scanf("%d",&n2);
     sub=n1-n2;
     printf("\n %d-%d=%d\n\n\n\n" ,n1, n2, sub);
+    //multiplicação e divisão
 
+
+    printf("Quer multiplicar [1] ou dividir? [2]:");
+    scanf("%d", &l);
+    fflush(stdin);
+
+
+
+
+
+       if(l==1)
+       {   s=n1=n2=0;
+           printf("\nDigite um numero inteiro:");
+           scanf("%d",&n1);
+           printf("\nDigite o segundo numero inteiro:");
+           scanf("%d",&n2);
+
+
+           s=n1*n2;
+           printf("\n%dx%d=%d\n\n\n\n", n1, n2, s);
+
+       }
+       else
+       {
+           s=n1=n2=0;
+           printf("\nDigite um numero:");
+           scanf("%f",&N1);
+           printf("\nDigite o segundo numero:");
+           scanf("%f",&N2);
+
+           S=(float)N1/N2;
+            printf("\n%f/%f=%f\n\n\n\n\n", N1, N2, S);
+
+       }
 
 
 
